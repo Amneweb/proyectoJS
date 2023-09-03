bb02aff2e27b5ca36362dc06f9f1258513d2caec
Amneris Calle <amneris.calle@gmail.com>
2023-09-02 23:30:39 -0300

    pasamos toda la programación al detrás de pantalla

--- scripts/entrega-1.js
+++ scripts/entrega-1.js
@@ -1,48 +1,86 @@
-let nombre_peli = "";
-function selectPeli() {
-    const peli = prompt("Por favor ingrese el número de la película elegida");
-    switch (peli) {
-        case "1":
-            nombre_peli = "Indiana Jones y el templo de la perdición";
-            break;
-        case "2":
-            nombre_peli = "Toy Story";
-            break;
-        default:
-            alert("El número que ingresaste no corresponde a ninguna película. Por favor intentalo de nuevo");
+function ejecutar() {
+    const funciones = [
+        ["Sábado 20/9 - 17:00",
+            "Sábado 20/9 - 18:30",
+            "Sábado 20/9 - 21:00",
+            "Domingo 21/9 - 14:00",
+            "Domingo 21/9 - 19:00"],
+        ["Sábado 20/9 - 16:00",
+            "Sábado 20/9 - 18:00",
+            "Sábado 20/9 - 19:50",
+            "Domingo 21/9 - 16:00",
+            "Domingo 21/9 - 19:00"],
+    ];
+    function mostrarFuncion(sala) {
+        let mostrar = "Acá te mostramos las funciones para dicha sala\n";
+        funciones[sala - 1].forEach((element, index) => mostrar = mostrar + "\n" + (index + 1) + " -> " + element);
+        alert(mostrar);
+    } 
+    function precioEntrada(sala, fila) {
+        switch (sala){
+            case 1:
+                if (fila<=8) {
+                    precio=$2900;
+                } else {
+                    precio=$3500;
+                }
+                console.log(precio);
+                break;
+            case 2:
+                if (fila<=8) {
+                    precio=$2200;
+                } else {
+                    precio=$2800;
+                }
+                break;    
+        }
+        return precio;
+            }
+    const peli = new Array();
+    peli[0] = "Titanic";
+    peli[1] = "Indiana Jones y el templo de la perdición";
+    peli[2] = "Toy Story";
+    peli[3] = "Volver al futuro";
+    peli[4] = "Africa mía";
+
+    let listarNombres = "Estas son las películas en cartelera\n";
+    for (let i = 0; i < 5; i++) {
+        listarNombres = listarNombres + (i + 1) + "->" + peli[i] + "\n";
     }
-    alert("Gracias por elegir " + nombre_peli + "Ahora podés continuar la compra.");
-    return nombre_peli;
-} 
-let sala="";
-function selectSala() {
-    sala = prompt("Ingresá el número correspondiente a la sala elegida");
-    if ((sala<1)||(sala>3)){
+    alert(listarNombres);
+    const peli_index = parseInt(prompt("Por favor ingrese el número de la película elegida"));
+    if ((peli_index < 1) || (peli_index > 5)) {
+        alert("El número que ingresaste no corresponde a ninguna película. Por favor intentalo de nuevo");
+        return;
+    } else {
+        alert("Gracias por elegir '" + peli[peli_index - 1] + "'. Ahora podés continuar la compra.");
+    }
+
+    let sala = "";
+    alert("SALAS Y PRECIOS\n**** 1-> Sala Premium****\nFilas 1 a 8: $2900\nFilas 9 a 20: $3500\n**** 2-> Sala Clásica****\nFilas 1 a 8: $2200\nFilas 9 a 20: $2800")
+    sala = parseInt(prompt("Ingresá el número correspondiente a la sala elegida"));
+    if ((sala < 1) || (sala > 2)) {
         alert("El número no corresponde a ninguna sala, por favor intentalo nuevamente");
+        return;
     } else {
         mostrarFuncion(sala);
     }
+ const nombreSala = (sala==1) ? "Sala Premium" : "Sala Clásica";
     
-}
-function precio(sala,fila) {
-
-}
-//en este array debería haber unas funciones para una sala y otras para otra sala. Pero por ahora lo dejo así.
-const funciones = [
-    "Sábado 20/9 - 17:00",
-    "Sábado 20/9 - 18:30",
-    "Sábado 20/9 - 21:00",
-    "Domingo 21/9 - 14:00",
-    "Domingo 21/9 - 19:00",
-];
-function mostrarFuncion(sala){
-    let mostrar = "Acá te mostramos las funciones para dicha sala\n";
-funciones.forEach((element,index)=> mostrar = mostrar + "\n" + (index+1) + " -> " + element);
-alert (mostrar);
-}
-
-let cantidad=0;
-function comprarEntradas () {
-cantidad=parseInt(prompt("¿Cuántas entradas querés comprar?"));
-
+    let cantidad = 0;
+    let fila = 0;
+    cantidad = parseInt(prompt("¿Cuántas entradas querés comprar?"));
+    fila=parseInt(prompt("Ingrese el número de fila"));
+    console.log(fila);
+    if (cantidad>10) {
+        alert("Lo sentimos la cantidad de entradas deseada (" + cantidad + ") excede la capacidad de la sala");
+        return;
+    }
+    if ((fila<1)||(fila>20)){
+        alert("El número de fila que ingresaste no corresponde a ninguna fila existente. Por favor volvé a iniciar el proceso y seleccioná una fila entre 1 y 20.");
+        return;
+    }
+    let a_pagar = precioEntrada(sala,fila) * cantidad;
+    console.log(a_pagar);
+    alert("Gracias por comprar tus entradas en Cine Vintage. Este es el resumen de tu compra:\n-> Película: " + peli[peli_index - 1] + "\n-> Sala: " + nombreSala + "\n-> Cantidad de entradas: " + cantidad + "\n-> Fila: " + fila + "\n-> TOTAL A PAGAR: " + a_pagar);
 }
