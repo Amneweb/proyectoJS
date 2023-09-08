3502a55666625a847ba9c8f0aeb40d0c2d3e6b34
Amneris Calle <amneris.calle@gmail.com>
2023-09-08 07:35:45 -0300

    Creación del Readme y corrección de estilos varios

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # proyectoJS
+## Idea general del proyecto final
+El proyecto consiste en una aplicación web de venta de entradas de un cine, con la opción de también agregar al carrito combos de snacks.
+Los pasos a seguir por el usuario serán:
+1 Elección de película
+1 Elección de sala (Premium, Clásica o Infantil)
+1 Elección de función (fecha y hora)
+1 Elección de asientos dependiendo de disponibilidad y cantidad de entradas
+1 Elección de ofertas de snacks
+1 Pago
+1 Generación de QR para entrada
+## Funcionalidades particulares en cada paso del recorrido del usuario
+**Pasos 1 a 3:** La elección de la película (con un select) dispara la aparición de las opciones del paso 2, que a su vez genera la aparición de un menú desplegable para el paso 3. Es decir que los horarios de las funciones dependerán de la sala elegida, y ésta a su vez dependerá de la película elegida. 
+**Paso 4:** Una vez definido el paso 3 le preguntaremos al usuario cuántas entradas quiere y, si la disponibilidad de asientos libres en la sala es igual o mayor a dicha cantidad, le mostraremos un plano de la sala con la ubicación de asientos libres. El usuario deberá hacer click en los asientos de preferencia.
+La idea es que en función de la ubicación de los asientos (y de la sala) el precio de la entrada varía.
+Para mostrar los asientos pensé en un *select* en el que cada opción es un círculo ubicado en la fila y columna correspondiente. Cuando el usuario hace click en el círculo queda seleccionado el asiento, cuyo id será, x ejemplo f10c4 (fila 10 columna 4)
+**Paso 5:** Se muestra el total a pagar hasta el momento y se ofrece la posibilidad de comprar snacks. Si el usuario desea comprar snacks, se le mostrarán las opciones y se sumará la elección al carrito de compras.
+**Paso 6:** Se simulará un pago (botón que diga pagar)
+**Paso 7:** Se genera un pdf con un QR (o por lo menos se muestra en pantalla)
+
+## Primera entrega
+### Disparador del simulador básico
+En esta primera entrega se trabaja sólo con prompts y alerts, aunque para que el programa empiece a funcionar hay que hacer click en un botón que se muestra en la página. (Hay dos botones que disparan el programa, son bastante obvios)
+TODO el proceso está dentro de una función denominada *ejecutar*, que es la que se dispara cuando se hace click en el botón. (Este tipo de funcionamiento lo copié de una de las primeras diapositivas de la clase 1 del curso).
+### Recorrido del usuario
+El primer prompt muestra 5 opciones de películas, cada una identificada con un número, que será el que el usuario deberá ingresar para elegir su película.
+A partir de ahí se van mostrando prompts con las opciones de sala y función, y finalmente se piden las cantidades de entradas y el número de fila.
+En base a las elecciones del usuario se calcula el total a pagar y se muestra en un alert final, con un resumen de 'la compra':
+-Película
+-Sala
+-Función
+-Cantidad de entradas
+-Fila
+-Total a pagar
+### Errores
+Si el usuario no ingresa ningún número, o ingresa opciones inválidas, se le muestra un alert diciéndole que hubo un error (y explicando cuál fue) y se suspende el proceso.
+
--- scripts/main.js
+++ scripts/main.js
@@ -15,30 +15,30 @@ function ejecutar() {
         let mostrar = "Acá te mostramos las funciones para dicha sala\n";
         funciones[sala - 1].forEach((element, index) => mostrar = mostrar + "\n" + (index + 1) + " -> " + element);
         return mostrar;
-    } 
+    }
     let precio = 0;
     function precioEntrada(sala, fila) {
-        switch (sala){
+        switch (sala) {
             case 1:
-                if (fila<=8) {
-                    precio=2900;
+                if (fila <= 8) {
+                    precio = 2900;
                 } else {
-                    precio=3500;
+                    precio = 3500;
                 }
                 break;
             case 2:
-                if (fila<=8) {
-                    precio=2200;
+                if (fila <= 8) {
+                    precio = 2200;
                 } else {
-                    precio=2800;
+                    precio = 2800;
                 }
-                break;    
+                break;
         }
         return precio;
-            }
-            function alertaVacio(variable) {
-                alert ("No ingresaste ningún valor de " + variable + ". Por favor volvá al inicio y cargá los datos nuevamente.")
-            }
+    }
+    function alertaVacio(variable) {
+        alert("No ingresaste ningún valor de " + variable + ". Por favor volvá al inicio y cargá los datos nuevamente.")
+    }
     const peli = new Array();
     peli[0] = "Titanic";
     peli[1] = "Indiana Jones y el templo de la perdición";
@@ -52,56 +52,56 @@ function ejecutar() {
     }
     const peli_index = parseInt(prompt(listarNombres + "Por favor ingrese el número de la película elegida"));
     if (peli_index) {
-    if ((peli_index < 1) || (peli_index > 5)) {
-        alert("El número que ingresaste no corresponde a ninguna película. Por favor intentalo de nuevo");
-        return;
+        if ((peli_index < 1) || (peli_index > 5)) {
+            alert("El número que ingresaste no corresponde a ninguna película. Por favor intentalo de nuevo");
+            return;
+        } else {
+            alert("Gracias por elegir '" + peli[peli_index - 1] + "'. Ahora podés continuar la compra.");
+        }
     } else {
-        alert("Gracias por elegir '" + peli[peli_index - 1] + "'. Ahora podés continuar la compra.");
+        alertaVacio("película");
+        return;
     }
-} else {
-    alertaVacio ("película");
-    return;
-}
     let sala = "";
     const lista_precios = "SALAS Y PRECIOS\n**** 1-> Sala Premium****\nFilas 1 a 8: $2900\nFilas 9 a 20: $3500\n**** 2-> Sala Clásica****\nFilas 1 a 8: $2200\nFilas 9 a 20: $2800\n";
     sala = parseInt(prompt(lista_precios + "Ingresá el número correspondiente a la sala elegida"));
     if (sala) {
-    if ((sala < 1) || (sala > 2)) {
-        alert("El número no corresponde a ninguna sala, por favor intentalo nuevamente");
+        if ((sala < 1) || (sala > 2)) {
+            alert("El número no corresponde a ninguna sala, por favor intentalo nuevamente");
+            return;
+        } else {
+            mostrar = mostrarFuncion(sala);
+            funcionElegida = prompt(mostrar + "\n¿Qué función elegís?");
+        }
+    } else {
+        alertaVacio("número de sala");
         return;
-    } else {
-        mostrar=mostrarFuncion(sala);
-        funcionElegida = prompt(mostrar + "\n¿Qué función elegís?");
     }
-    } else {
-    alertaVacio ("número de sala");
-    return;
-}
- const nombreSala = (sala==1) ? "Premium" : "Clásica";
-    
+    const nombreSala = (sala == 1) ? "Premium" : "Clásica";
+
     let cantidad = 0;
     let fila = 0;
-    cantidad = parseInt(prompt("¿Cuántas entradas querés comprar?"));
+    cantidad = parseInt(prompt("¿Cuántas entradas querés comprar? El máximo disponible son 10 entradas."));
     if (cantidad) {
-    if (cantidad>10) {
-        alert("Lo sentimos la cantidad de entradas deseada (" + cantidad + ") excede la capacidad de la sala");
+        if (cantidad > 10) {
+            alert("Lo sentimos la cantidad de entradas deseada (" + cantidad + ") excede la capacidad de la sala");
+            return;
+        }
+    } else {
+        alertaVacio("cantidad de entradas");
         return;
     }
-    } else {
-       alertaVacio("cantidad de entradas");
-       return;
-    }
-    fila=parseInt(prompt("Ingrese el número de fila"));
+    fila = parseInt(prompt("Ingrese el número de fila. Hay un máximo de 20 filas."));
     if (fila) {
-    
-    if ((fila<1)||(fila>20)){
-        alert("El número de fila que ingresaste no corresponde a ninguna fila existente. Por favor volvé a iniciar el proceso y seleccioná una fila entre 1 y 20.");
+
+        if ((fila < 1) || (fila > 20)) {
+            alert("El número de fila que ingresaste no corresponde a ninguna fila existente. Por favor volvé a iniciar el proceso y seleccioná una fila entre 1 y 20.");
+            return;
+        }
+    } else {
+        alertaVacio("número de fila");
         return;
     }
-    } else {
-       alertaVacio ("número de fila");
-       return;
-}
-    let a_pagar = precioEntrada(sala,fila) * cantidad;
-    alert("Gracias por comprar tus entradas en Cine Vintage. Este es el resumen de tu compra:\n-> Película: " + peli[peli_index - 1] + "\n-> Sala: " + nombreSala + "\n-> Funcion: " + funciones[sala-1][funcionElegida-1] + "\n-> Cantidad de entradas: " + cantidad + "\n-> Fila: " + fila + "\n-> TOTAL A PAGAR: $" + a_pagar);
+    let a_pagar = precioEntrada(sala, fila) * cantidad;
+    alert("Gracias por comprar tus entradas en Cine Vintage. Este es el resumen de tu compra:\n-> Película: " + peli[peli_index - 1] + "\n-> Sala: " + nombreSala + "\n-> Funcion: " + funciones[sala - 1][funcionElegida - 1] + "\n-> Cantidad de entradas: " + cantidad + "\n-> Fila: " + fila + "\n-> TOTAL A PAGAR: $" + a_pagar);
 }
