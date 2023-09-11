a6573714fcfe9c4168b926da2a6e9b1f852cd301
Amneris Calle <amneris.calle@gmail.com>
2023-09-10 21:24:32 -0300

    acomodamos el estilo y agregamos posters de películas

--- scripts/practicas.js
+++ scripts/practicas.js
@@ -20,3 +20,22 @@ if (edadUsuario < 18) {
 } 
 alert (datosUsuario);
 const MI_NOMBRE = "JS by Amne";
+
+const PRODUCTO1="bicicleta";
+const PRODUCTO2="triciclo";
+const PRODUCTO3="monopatín";
+//quiero mostrar por consola los 3 valores de las variables
+//pero no quiero escribir console.log 3 veces
+//entonces uso un for. ¿Cómo hago para que el código entienda
+//que lo que yo quiero mostrar es el contenido de las variables 
+//cuyo nombre está formado por PRODUCTO y el valor de i?
+for (let i=1; i<4; i++) {
+console.log("PRODUCTO"+i);
+}
+//en este caso muestra PRODUCTO1, PRODUCTO2, PRODUCTO3
+for (let i=1; i<4; i++) {
+    console.log(PRODUCTO+i);
+    }
+//en este caso da error porque la variable producto no está
+//definida
+chrome.runtime.sendMessage('ping').then(response => {  })
